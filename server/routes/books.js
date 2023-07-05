ef88abcb81c1488d712fbd21f7b8ecb4806e76d2
IssacYim14 <134939017+IssacYim14@users.noreply.github.com>
2023-07-05 13:39:31 -0400

    Fix the Add Button

--- server/routes/books.js
+++ server/routes/books.js
@@ -25,11 +25,13 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
     /*****************
      * ADD CODE HERE *
      *****************/
-
+    res.render("books/details",{
+      title:'Book Details',
+      books: ''
+    });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
